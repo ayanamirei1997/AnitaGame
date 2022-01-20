42dda3036d5eca25400bff3e319ce42264c3768a
ayanamirei1997 <wanghaoran1997@yahoo.com>
2022-01-20 22:59:04 +0800

    add vfx

---
+++ Assets/Anita/Sources/Core/VFX/ShaderPropertyType.cs
@@ -0,0 +1,11 @@
+namespace Anita
+{
+    // Drop some items from UnityEditor.ShaderUtil.ShaderPropertyType
+    public enum ShaderPropertyType
+    {
+        Color,
+        Vector,
+        Float,
+        TexEnv
+    }
+}
