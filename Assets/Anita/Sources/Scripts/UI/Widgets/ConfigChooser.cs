1fa8d8beaba8ab63efbee9cf84152825a0445f68
ayanamirei1997 <wanghaoran1997@yahoo.com>
2022-01-20 23:41:03 +0800

    add widgets

---
+++ Assets/Anita/Sources/Scripts/UI/Widgets/ConfigChooser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Anita
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumDisplayNameAttribute : Attribute
+    {
+        public readonly string displayName;
+
+        public EnumDisplayNameAttribute(string displayName)
+        {
+            this.displayName = displayName;
+        }
+    }
+
+    /// <summary>
+    /// Use radio group to modify the value in ConfigManager
+    /// </summary>
+    [RequireComponent(typeof(ToggleGroup), typeof(LayoutGroup))]
+    public class ConfigChooser : MonoBehaviour
+    {
+        public string configKeyName;
+        public string enumTypeName;
+        public GameObject togglePrefab;
+
+        private ToggleGroup toggleGroup;
+        private List<Toggle> toggles;
+        private ConfigManager configManager;
+
+        private void Awake()
+        {
+            this.RuntimeAssert(!string.IsNullOrEmpty(configKeyName), "Empty configKeyName.");
+
+            toggleGroup = GetComponent<ToggleGroup>();
+
+            configManager = Utils.FindAnitaGameController().ConfigManager;
+            var configEnum = Type.GetType("Anita." + enumTypeName);
+            if (configEnum != null && configEnum.IsEnum)
+            {
+                toggles = Enum.GetNames(configEnum).Select(name =>
+                {
+                    var attrs = configEnum.GetMember(name)[0]
+                        .GetCustomAttributes(typeof(EnumDisplayNameAttribute), false);
+                    var displayName = attrs.Length > 0 ? (attrs[0] as EnumDisplayNameAttribute).displayName : name;
+                    var go = Instantiate(togglePrefab, transform);
+                    go.GetComponentInChildren<Text>().text = displayName;
+                    var toggle = go.GetComponentInChildren<Toggle>();
+                    toggle.group = toggleGroup;
+                    return toggle;
+                }).ToList();
+                toggleGroup.SetAllTogglesOff();
+            }
+        }
+
+        private void UpdateValue()
+        {
+            var value = configManager.GetInt(configKeyName);
+            if (toggles.Count > value && value >= 0 && toggles[value].isOn)
+            {
+                // Eliminate infinite recursion
+                return;
+            }
+
+            toggles[value].isOn = true;
+        }
+
+        private void OnValueChange(bool _)
+        {
+            for (int i = 0; i < toggles.Count; i++)
+            {
+                if (toggles[i].isOn)
+                {
+                    configManager.SetInt(configKeyName, i);
+                    return;
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            foreach (var toggle in toggles)
+            {
+                toggle.onValueChanged.AddListener(OnValueChange);
+            }
+
+            configManager.AddValueChangeListener(configKeyName, UpdateValue);
+            UpdateValue();
+        }
+
+        private void OnDisable()
+        {
+            foreach (var toggle in toggles)
+            {
+                toggle.onValueChanged.RemoveListener(OnValueChange);
+            }
+
+            configManager.RemoveValueChangeListener(configKeyName, UpdateValue);
+        }
+    }
+}
