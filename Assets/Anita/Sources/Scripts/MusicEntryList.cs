a3238ad8afd9f0a13251c5c6eb402bebc6ba1595
ayanamirei1997 <wanghaoran1997@yahoo.com>
2022-01-23 23:22:56 +0800

    add audio

---
+++ Assets/Anita/Sources/Scripts/MusicEntryList.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anita
+{
+    public class MusicEntryList : ScriptableObject
+    {
+        public List<MusicEntry> entries;
+    }
+}
