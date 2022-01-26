5816c0861669ac9a1789fabc2543367db996a562
ayanamirei1997 <wanghaoran1997@yahoo.com>
2022-01-26 20:22:09 +0800

    add ImageGallery

---
+++ Assets/Anita/Sources/Scripts/UI/ImageGallery/ImageGroup.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anita
+{
+    public class ImageGroup : ScriptableObject
+    {
+        public List<ImageEntry> entries;
+    }
+}
---
+++ Assets/Anita/Sources/Scripts/UI/ImageGallery/ImageGroupList.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anita
+{
+    public class ImageGroupList : ScriptableObject
+    {
+        public List<ImageGroup> groups;
+    }
+}
